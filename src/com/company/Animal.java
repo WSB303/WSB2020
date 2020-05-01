dc8066dca48611dc41cf50c91a9e46f712d30288
Kinga <c.m97@o2.pl>
2020-05-01 16:36:20 +0200

    commit 1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -3,9 +3,36 @@ package com.company;
 public class Animal {
     final String species;
     String name;
-    private Double weight = 5.0;
+    private Double weight;
+
+    public static final Double DEFAULT_DOG_WEIGHT = 5.0;
+    public static final Double DEFAULT_MOUSE_WEIGHT = 0.1;
+    public static final Double DEFAULT_LION_WEIGHT = 123.0;
     public Animal(String species)
     {
         this.species = species;
+        switch(species) {
+            case "dog":
+                this.weight = DEFAULT_DOG_WEIGHT;
+                break;
+            case "mouse":
+                this.weight = DEFAULT_MOUSE_WEIGHT;
+                break;
+            case "lion":
+                this.weight = DEFAULT_LION_WEIGHT;
+                break;
+        }
+    }
+    public void feed() {
+        if(weight>0.0){
+            weight++;
+            System.out.println("Thanks for food! Actual weight:" +weight+"kg");
+        }else System.out.println("You can't feed dead animal.");
+    }
+    public void walk(){
+        if(weight>0.0){
+            weight--;
+            System.out.println("Thanks for walk! Actual weight:"+weight+"kg");
+        }else System.out.println("You walking with dead animal...");
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,28 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("elo");
+        Human me = new Human();
+        me.firstName = "George";
+        me.lastName = "Flinston";
+
+
+        Animal dog = new Animal("dog");
+        dog.name = "Kaja";
+
+        me.pet = dog;
+
+
+        dog.feed();
+        dog.walk();
+        dog.feed();
+        dog.walk();
+        dog.walk();
+        dog.walk();
+        dog.walk();
+        dog.walk();
+        dog.walk();
+        dog.walk();
+        dog.feed();
+
     }
 }

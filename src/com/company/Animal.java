ffbc93cc36b76cf5574fb6e30942bf3dcea566c1
Kinga <c.m97@o2.pl>
2020-05-01 14:43:15 +0200

    Initial commit

---
+++ src/com/company/Animal.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Animal {
+    final String species;
+    String name;
+    private Double weight = 5.0;
+
+    public Animal(String species)
+    {
+        this.species = species;
+    }
+}
